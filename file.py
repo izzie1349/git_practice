0554b3c42f738eea7ac9781b0d93935d7a1b785d
israel <izzie1349@gmail.com>
2015-08-21 21:53:07 -0400

    branch1 blabla

--- file.py
+++ file.py
@@ -2,35 +2,15 @@
 this is a file im going to use to
 practice git with!
 
-this is my first change
-"""
-
-"""
-fix a
 """
 
 
-
-"""
-this is fix b
-all of this in
-this in the comments
-is part of B!
-"""
+this is branch1:
+im going to make a bunch of 
+changes on here to practice 
+merging and pushing to the
+remote repository
 
 
-"""
-im going to create a branch m2m
-merge that to master
-then push master to master!
-"""
 
 
-"""
-branch and push to remote
-
-"""
-
-"""
-branch to remote again!
-"""
